2facb589b1fd84d742b81e008f62fa7bf277894a
Julia Lahdo <44289405+JuliaLahdo@users.noreply.github.com>
2019-05-02 14:45:04 +0200

    Revert "Show products"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ShowproductsComponent } from './showproducts/showproducts.component';
 
-const routes: Routes = [
-  {path: '', component: ShowproductsComponent}
-  // {path: 'about', component: AboutComponent},
-  // {path: 'details/:id', component: DetailsComponent},
-  // {path: '**', component: NotfoundComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
